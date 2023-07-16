336459f8853e88689806705b18f182610549781a
Muhammad-Tahir <mtz5prif@gmail.com>
2023-07-16 20:32:41 +0100

    add auth middleware

--- server/middlewares/verifySignUp.ts
+++ server/middlewares/verifySignUp.ts
@@ -1,60 +1,65 @@
-// import db from "../models";
+import db from "../models";
+import { Request, Response, NextFunction } from "express";
 
-// const ROLES = db.ROLES;
-// const User = db.user;
+const ROLES = db.ROLES;
+const User = db.user;
 
-// const checkDuplicateUsernameOrEmail = (req, res, next) => {
-//   // Username
-//   User.findOne({
-//     username: req.body.username,
-//   }).exec((err, user) => {
-//     if (err) {
-//       res.status(500).send({ message: err });
-//       return;
-//     }
+const checkDuplicateUsernameOrEmail = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  User.findOne({
+    username: req?.body?.username ,
+  })
+    .exec()
+    .then((_response) => {
+      res?.status(400).send({ message: "Failed! Username is already in use!" });
 
-//     if (user) {
-//       res.status(400).send({ message: "Failed! Username is already in use!" });
-//       return;
-//     }
+      User.findOne({
+        email: req?.body?.email,
+      })
+        .exec()
+        .then((_response) => {
+          res
+            ?.status(400)
+            .send({ message: "Failed! Email is already in use!" });
+          next();
 
-//     // Email
-//     User.findOne({
-//       email: req.body.email,
-//     }).exec((err, user) => {
-//       if (err) {
-//         res.status(500).send({ message: err });
-//         return;
-//       }
+          return;
+        })
+        .catch((err) => {
+          res.status(500).send({ message: err });
+          return;
+        });
 
-//       if (user) {
-//         res.status(400).send({ message: "Failed! Email is already in use!" });
-//         return;
-//       }
+      next();
+      return;
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err });
+      return;
+    });
+};
 
-//       next();
-//     });
-//   });
-// };
+const checkRolesExisted = (req: Request, res: Response, next: NextFunction) => {
+  if (req.body.roles) {
+    for (let i = 0; i < req.body.roles.length; i++) {
+      if (!ROLES.includes(req.body.roles[i])) {
+        res.status(400).send({
+          message: `Failed! Role ${req.body.roles[i]} does not exist!`,
+        });
+        return;
+      }
+    }
+  }
 
-// const checkRolesExisted = (req, res, next) => {
-//   if (req.body.roles) {
-//     for (let i = 0; i < req.body.roles.length; i++) {
-//       if (!ROLES.includes(req.body.roles[i])) {
-//         res.status(400).send({
-//           message: `Failed! Role ${req.body.roles[i]} does not exist!`,
-//         });
-//         return;
-//       }
-//     }
-//   }
+  next();
+};
 
-//   next();
-// };
+const verifySignUp = {
+  checkDuplicateUsernameOrEmail,
+  checkRolesExisted,
+};
 
-// const verifySignUp = {
-//   checkDuplicateUsernameOrEmail,
-//   checkRolesExisted,
-// };
-
-// export default verifySignUp;
+export default verifySignUp;
