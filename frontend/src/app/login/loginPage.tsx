4a2c04ad2b4c79e1a9d0e8698a3b792850c85846
Muhammad-Tahir <mtz5prif@gmail.com>
2023-07-22 12:19:46 +0100

    refactor login page

--- frontend/src/app/login/loginPage.tsx
+++ frontend/src/app/login/loginPage.tsx
@@ -1,126 +1,88 @@
 import tw from "tailwind-styled-components";
 import Link from "next/link";
-import google from "../../assets/google.png";
-import apple from "../../assets/apple.png";
-// import username from "../../images/username.png";
-// import password from "../../images/password.png";
+import google from "@/assets/google.png";
+import apple from "@/assets/apple.png";
+
 import Image from "next/image";
+import Input from "@/components/Input";
+import Text from "@/components/Text";
 
 export default function LoginPage() {
   return (
-    <div>
-      <Root2>
-        <Custom_div>
-          <Custom_div_2>
-            <Custom_div_3>
-              <Custom_h1>You must sign in to join</Custom_h1>
-            </Custom_div_3>
-            <Custom_div_3>
-              <Custom_h1_2>We are a team that guides each other </Custom_h1_2>
-            </Custom_div_3>
+    <Root>
+      <WhiteBg>
+        <div className="flex flex-col items-center justify-center">
+          <Text variant="H3" color="text-gray-600">
+            You must Sign In to join
+          </Text>
+          <Text variant="H5" color="text-gray-400">
+            We&apos;re a team that guides each other
+          </Text>
+        </div>
 
-            <div className=" px-5 flex  items-center  justify-center  bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-              <Image src={google} alt={""} width={15} className="mr-3" />
+        <div className="w-full flex flex-col gap-3 mt-6">
+          <ThirdPartySignInLink
+            href="/"
+          >
+            <Image src={google} alt={""} width={27} height={24} />
+            <Text variant="H4" color="text-gray-700">
+              Sign in with Google
+            </Text>
+          </ThirdPartySignInLink>
 
-              <Link href="/" id="google">
-                Sign in with Google{" "}
-              </Link>
-            </div>
+          <ThirdPartySignInLink
+            href="/"
+          >
+            <Image src={apple} alt={""} width={27} height={27} />
+            <Text variant="H4" color="text-gray-700">
+              Sign in with Google
+            </Text>
+          </ThirdPartySignInLink>
+        </div>
 
-            <div className=" px-5 flex  items-center  justify-center  bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-              <Image src={apple} alt={""} width={25} className="mr-3" />
-              <Link href="/" id="apple">
-                Sign in with Apple{" "}
-              </Link>
-            </div>
+        <div className="relative mt-4">
+          <TextBetweenLines
+            variant="p1"
+            color="text-gray-400"
+          >
+            Or
+          </TextBetweenLines>
+        </div>
 
-            <Custom_div_4>
-              <Custom_div_5></Custom_div_5>
-              <Custom_span>or</Custom_span>
-              <Custom_div_5></Custom_div_5>
-            </Custom_div_4>
+        <form className="space-y-2 w-full" action="#">
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Uname@mail.com"
+            label="Email or username"
+          />
 
-            <form className="space-y-4 md:space-y-6" action="#">
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-400"
-              >
-                Email or username
-              </label>
-              {/* <Image src={username} alt={""} width={15} className="mr-2" /> */}
-              <Custom_input style={{ marginTop: "-0.5em" }}>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Uname@mail.com"
-                  // className=" flex  items-left    bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                />
-              </Custom_input>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            label="Password"
+          />
+        </form>
 
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-400"
-              >
-                Password
-              </label>
-              {/* <Image src={password} alt={""} width={15} className="mr-2" /> */}
-              <Custom_input style={{ marginTop: "-0.1em" }}>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="password"
-                  // className=" flex  items-left    bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                />
-              </Custom_input>
-              <div className=" flex items-center justify-end">
-                <Link
-                  style={{ marginTop: "-1em" }}
-                  href="/forgotPassword"
-                  className="text-sm font-medium text-blue-600 hover:underline "
-                >
-                  Forgot password?
-                </Link>
-              </div>
-              <Link
-                href="/"
-                id="google2"
-                className="flex  items-center  justify-center bg-blue-700  border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-              >
-                Sign in with Google{" "}
-              </Link>
-            
-              <Custom_div_3>
-                <Custom_p>
-                  Don’t have an account?{" "}
-                  <a
-                    href="#"
-                    className="font-medium text-primary-600 hover:underline "
-                  >
-                    Sign up
-                  </a>
-                </Custom_p>
-              </Custom_div_3>
-            </form>
-          </Custom_div_2>
-        </Custom_div>
-      </Root2>
-    </div>
+        <ForgotPasswordLink href='/forgot-password'>Forgot password?</ForgotPasswordLink>
+
+        <SubmitButton type='submit'>Sign In</SubmitButton>
+      </WhiteBg>
+    </Root>
   );
 }
 
-// const Forgot_password = tw.div`flex items-center justify-end"`;
+const WhiteBg = tw.div` w-full bg-white rounded-lg shadow  sm:max-w-md p-10 flex flex-col justify-center items-center`;
 
-const Custom_p = tw.div`text-sm font-light text-gray-500`;
-// const Custom_Link = tw.link`flex  items-center  justify-center bg-blue-700  border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`;
-const Custom_input = tw.div`flex  items-left   bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`;
-const Custom_span = tw.div`flex-shrink mx-4 text-gray-400`;
-const Custom_h1_2 = tw.div`text-sm  leading-tight tracking-tight text-gray-400`;
-const Custom_h1 = tw.div`text-xl  leading-tight tracking-tight text-gray-900`;
-const Custom_div_5 = tw.div`flex-grow border-t border-gray-400"`;
-const Custom_div_4 = tw.div`relative flex py-4 items-center`;
-const Custom_div_3 = tw.div`flex  items-center  justify-center`;
-const Custom_div_2 = tw.div`p-6 space-y-4 md:space-y-6 sm:p-8`;
-const Custom_div = tw.div` w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0`;
-const Root2 = tw.div`flex items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0`;
+const Root = tw.div`flex items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0`;
+
+const ThirdPartySignInLink = tw(Link)`border border-gray-400 w-full flex rounded-md gap-4 justify-center px-2 py-1.5`;
+
+const ForgotPasswordLink = tw(Link)`self-end text-blue-400 cursor-pointer hover:text-blue-800 text-[14px]`;
+
+const TextBetweenLines = tw(Text)`my-4 before:absolute before:right-[100%] before:top-[calc(50%_-_0.5px)] before:mr-3 before:w-[100px] before:h-[1px] before:bg-gray-300 after:absolute after:left-[100%] after:top-[calc(50%_-_0.5px)] after:ml-3 after:w-[100px] after:h-[1px] after:bg-gray-300`;
+
+const SubmitButton=tw.button`text-white bg-sky-600 hover:bg-blue-800 w-full py-3 flex items-center justify-center rounded-[6px] shadow-md mt-6`
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,12 +1,10 @@
-import tw from 'tailwind-styled-components';
-import LoginPage from './loginPage';
+import tw from "tailwind-styled-components";
+import LoginPage from "./loginPage";
 
 export default function Login() {
   return (
     <Root>
-      <h2 className='text-white'>Login Page</h2>
-      {/* Add your login form here */}
-      <LoginPage/>
+      <LoginPage />
     </Root>
   );
 }
